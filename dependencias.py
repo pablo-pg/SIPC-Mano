3c145808f8a96ac94e72a48371e6fbf32f08e1a6
PabloPG <alu0101318318@ull.edu.es>
2022-01-16 19:36:33 +0000

    Puesto bonito

--- dependencias.py
+++ dependencias.py
@@ -1,16 +1,7 @@
-from typing import Tuple
 import numpy as np
 import cv2
 import time
 import math
-
-cap = cv2.VideoCapture(0)
-backSub = cv2.createBackgroundSubtractorMOG2(detectShadows = True)
-image = cv2.imread('media/hand.JPG')
-# Convertir imagen en escala de grises
-gray = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY) 
-# Pasa los pixeles que esten por encima de 127 los pasa a 255 y el resto a 0
-ret,bw = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)
 
 
 # Comprueba si una lista está ordenada de mayor a menor
@@ -54,19 +45,12 @@ def angle(s,e,f):
 # En el caso de que el usuario quiera usar el programa con su propia cámara.
 # Este método se encarga de la gestión de la cámara únicamente.
 def abrirCamara():
+	cap = cv2.VideoCapture(0)
 	if not cap.isOpened:
 		print ("Unable to open cam")
 		exit(0)
-	while (True):
-		ret,frame=cap.read()
-		generarCuadrado(cap)     #< Hace todo lo referente a fgMask, defectos de convexidad, etc.
-		if not ret:
-			exit(0)
-		cv2.imshow('frame',frame)
-		keyboard = cv2.waitKey(1)
-		if keyboard & 0xFF == ord('q'):
-			break
-
+	
+	generarCuadrado(cap)     #< Hace todo lo referente a fgMask, defectos de convexidad, etc.
 	cap.release()
 	cv2.destroyAllWindows()
 
@@ -78,16 +62,8 @@ def abrirVideo(video):
 	if not cap.isOpened:
 		print ("Unable to open file")
 		exit(0)
-	while (True):
-		ret,frame=cap.read()
-		generarCuadrado(cap)     #< Hace todo lo referente a fgMask, defectos de convexidad, etc.
-		if not ret:
-			exit(0)
-		cv2.imshow('frame',frame)
-		keyboard = cv2.waitKey(40)
-		if keyboard & 0xFF == ord('q'):
-			break
-
+	
+	generarCuadrado(cap)     #< Hace todo lo referente a fgMask, defectos de convexidad, etc.
 	cap.release()
 	cv2.destroyAllWindows()
 
@@ -97,32 +73,43 @@ def generarCuadrado(cap):
 	if not cap.isOpened:
 		print ("Unable to open file")
 		exit(0)
+	
 	# Las dimensiones del rectángulo
 	pt1 = (390,90)
 	pt2 = (590,310)
-  # Por defecto la máscara se actualiza
+
+	# Se configura la forma de sustraer el fondo
+	backSub = cv2.createBackgroundSubtractorMOG2(detectShadows = True)
+
+  # Por defecto la máscara se actualiza y no se pinta en la pantalla
 	learning_rate = -1
 	imprimir_puntos = False
 	todos_puntos = []
+	# Empieza el tratamiento de la información
 	while (True):
 		ret,frame=cap.read()
 		if not ret:
 			exit(0)
 		
+		# Se crea un rectangulo donde se va a trabajar
 		roi = frame[pt1[1]:pt2[1],pt1[0]:pt2[0],:].copy()
 		cv2.rectangle(frame,pt1,pt2,(255,0,0))
+
 		# Se aplica la máscara
 		fgMask = backSub.apply(roi,learningRate=learning_rate)
 		cv2.imshow('frame',frame)
 		cv2.imshow('Foreground Mask',fgMask)
+
 		# Se hace todo lo relacionado con los defectos de convexidad (conteo de dedos, detección de gestos, etc.)
 		nuevo_punto = convDefects(roi, fgMask, todos_puntos, imprimir_puntos)
+		# En el caso de que se haya detectado un dedo y esté activado el modo pintar, se añadirá el punto a la lista de puntos independientes del fotograma
 		if nuevo_punto.__class__ == tuple:
 			if imprimir_puntos == True:
 				if nuevo_punto[0].__class__ == np.int32:
 					todos_puntos.append(nuevo_punto)
 
 		keyboard = cv2.waitKey(1)
+		# Si se pulsa la q, se sale
 		if keyboard & 0xFF == ord('q'):
 			break
 		# Si se pulsa la s, deja de actualizar la máscara
@@ -131,8 +118,10 @@ def generarCuadrado(cap):
 		# Si se pulsa la a, vuelve a actualizar la máscara
 		elif keyboard & 0xFF == ord('a'):
 			learning_rate = -1
+		# Si se pulsa la p, se entra en el modo de pintar
 		elif keyboard & 0xFF == ord('p'):
 			imprimir_puntos = True
+		# Si se pulsa la b, se borra lo pintado y se sale del modo pintar
 		elif keyboard & 0xFF == ord('b'):
 			todos_puntos = []
 			imprimir_puntos = False
@@ -145,11 +134,14 @@ def generarCuadrado(cap):
 #   * Calcula los defectos de convexidad
 #   * Cuenta los dedos
 #   * Identifica los gestos
+#   * Pinta
 def convDefects(frame, fgmask, todos_puntos, imprimir_puntos):
 	finger_cnt = 0  # Contador inicial de cuántos dedos hay
-	nuevo_punto = 0
+	nuevo_punto = 0 # Punto que retornará en el caso de que esté en modo pintar
+
 	# Se obtienen los contornos de la mano según el fgMask
 	contours, hierarchy = cv2.findContours(fgmask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2:]
+
 	# De todos los contornos que puedan haber, nos quedamos solo con el más grande (la mano)
 	if (len(contours) > 1):
 		max = np.copy(contours[0]) 
@@ -157,20 +149,27 @@ def convDefects(frame, fgmask, todos_puntos, imprimir_puntos):
 			if (len(i) > len(max)):
 				max = np.copy(i)
 		cnt = np.copy(max)
+
 		# Se muestra el borde de la mano
 		cv2.drawContours(frame, [cnt], -1, (0,255,0),3)
+
 		# Se calcula el bounding rect, es decir, un rectángulo cuyos bordes coinciden con los bordes de la mano
 		rect = cv2.boundingRect(cnt)
 		pt1 = (rect[0],rect[1])
 		pt2 = (rect[0]+rect[2],rect[1]+rect[3])
+		cv2.rectangle(frame,pt1,pt2,(0,0,255),3)
+
 		# En el caso de que el contorno sea suficientemente grande, se tratará como si fuera la mano
 		if (len(cnt) > 4):
 			hull = cv2.convexHull(cnt, returnPoints=False)
+
 			# Existe la posibilidad de que el hull esté desordenado, haciendo que falle convexityDefects(), por lo que se ordena
 			mysort(hull)
 			# time.sleep(0.02)
-			# Se obtienen todos los defectos de convexidad
+
+			# Se obtienen todos los defectos de convexidad de la mano
 			defects = cv2.convexityDefects(cnt,hull)
+
 			# En el caso de que hayan defectos de convexidad, es decir, defects no sea de tipo NoneType
 			if defects.__class__ == np.ndarray:
 				for i in range(len(defects)):
@@ -181,29 +180,33 @@ def convDefects(frame, fgmask, todos_puntos, imprimir_puntos):
 					far = tuple(cnt[f][0])
 					depth = d/256.0
 					ang = angle(start,end,far)
+
+					# Se calcula el tamaño horizontal y vertical del rectangulo
 					vertical_size = abs((pt2[1] - pt1[1]))
 					horizontal_size = abs((pt2[0] - pt1[0]))
-					# En el caso de que el ángulo del defecto sea menor de 90º y tenga una longitud mayor de 50, será un dedo
+
+					# En el caso de que el ángulo del defecto de convexidad sea menor de 90º y tenga una longitud mayor de 50p, será un dedo
 					if  ang <= 90:
 						if depth > 50:
 							finger_cnt += 1
 							cv2.circle(frame, far, 4, [0, 0, 255], -1)
 							cv2.line(frame,start,end,[255,0,0],2)
+
 				# Si hay más de un dedo, empiezan a detectarse posibles gestos
 				if finger_cnt > 0:
 					finger_cnt = finger_cnt + 1
-					distancia_dedos = np.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
-					# end es el dedo de la izquierda
-					# start es el dedo de la derecha
-					# cv2.circle(frame, pt1, 10, [0, 0, 0], -1)
+
+					# end es el dedo de la izquierda, start es el dedo de la derecha
 					cv2.circle(frame, start, 10, [255, 255, 0], -1)
 					cv2.circle(frame, end, 10, [255, 255, 255], -1)
+
 					# Se calculan las distancias de las puntas de los dedos con los bordes del bounding rect
 					distY_d_izq_esq_sup_izq = abs(end[1] - pt1[1]) / vertical_size * 100
 					distY_d_der_esq_sup_izq = abs(start[1] - pt1[1]) / vertical_size * 100
 					distX_d_izq_esq_sup_izq = abs(end[0] - pt1[0]) / horizontal_size * 100
 					distX_d_der_esq_sup_izq = abs(start[0] - pt1[0]) / horizontal_size * 100
 					# print(f'{distY_d_izq_esq_sup_izq}  -  {distY_d_der_esq_sup_izq}  -  {distX_d_izq_esq_sup_izq} - {distX_d_der_esq_sup_izq}')
+					
 					# Gesto de la paz
 					if finger_cnt == 2 and vertical_size > horizontal_size * 1.2:
 						if distY_d_izq_esq_sup_izq < 11 and distY_d_der_esq_sup_izq < 11 and distX_d_izq_esq_sup_izq < 25:
@@ -214,29 +217,33 @@ def convDefects(frame, fgmask, todos_puntos, imprimir_puntos):
 								cv2.putText(frame, str("Paz hermano"), (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0) , 2, cv2.LINE_AA)
 								nuevo_punto = start
 
-					# Gesto satánico
+					# Gesto rockero
 					if finger_cnt == 3:
 						if distY_d_izq_esq_sup_izq < 2 and distY_d_der_esq_sup_izq < 25 and distX_d_izq_esq_sup_izq < 45 and distX_d_der_esq_sup_izq < 100:
 							if distY_d_izq_esq_sup_izq >= 0 and distY_d_der_esq_sup_izq > 10 and distX_d_izq_esq_sup_izq > 15 and distX_d_der_esq_sup_izq > 61:
 								cv2.putText(frame, str("Rock & Roll"), (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0) , 2, cv2.LINE_AA)
+
 				# En el caso de que no hayan defectos de convexidad, es posible que se esté levantando solo un dedo.
 				elif finger_cnt == 0:
 					# En el caso de levantar el dedo índice, corazón, anular o meñique
 					if vertical_size / horizontal_size > 1.5:
 						finger_cnt = finger_cnt + 1
+
 					# En el caso de levantar el dedo pulgar
 					elif horizontal_size / vertical_size > 1.2:
 						finger_cnt = finger_cnt + 1
-		cv2.rectangle(frame,pt1,pt2,(0,0,255),3)
-		
+
+	# Se imprime el número de dedos en la imagen	
 	cv2.putText(frame, str(finger_cnt), (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0) , 2, cv2.LINE_AA)
+	
+	# En el caso de que esté activado el modo pintar, se imprimen todos los puntos almacenados
 	if (imprimir_puntos == True):
 		if nuevo_punto.__class__ == tuple:
 			if nuevo_punto[0].__class__ == np.int32:
 				todos_puntos.append(nuevo_punto)
-		# print(len(todos_puntos))
 		for i in todos_puntos:
 			cv2.circle(frame, i, 10, [150, 55, 50], -1)
+	
 	cv2.imshow('Contours',frame)
 	return nuevo_punto
 
